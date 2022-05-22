e2b7c6d644ca29bec7d49fd0de95dc9d181f198a
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-05-22 21:26:49 +0530

    [Bhagya] wages for month

--- empwage.sh
+++ empwage.sh
@@ -1,19 +1,31 @@
 #! /bin/bash -x
 
+parttime=2
+parttimehour=8
 present=1
 absent=0
 wageperhour=20
 fulldayhour=8
-empstatus=$((RANDOM % 2))
+wagepermonth=0
+empstatus=$((RANDOM % 3))
 
 echo "Welcome to Emplopyee wage computation "
+for((i=0;i<20;i++))
+do
+	if [ $present -eq $empstatus ]
+	then
+		wageperday=$(($fulldayhour*$wageperhour))
+		echo "employee is present"
+	elif [ $absent -eq $empstatus ]
+	then
+		wageperday=0
+		echo "employee is absent"
+	elif [ $parttime -eq $empstatus ]
+	then
+		wageperday=$(($parttimehour*$wageperhour))
+		echo "employee is parttime"
+	fi
+	wagepermonth=$(($wagepermonth+$wageperday))
+done
 
-if [ $present -eq $empstatus ]
-then
-	wageperday=$(($fulldayhour*$wageperhour))
-	echo "employee is present"
-elif [ $absent -eq $empstatus ]
-then
-	echo "employee is absent"
-fi
-
+echo $wagepermonth
