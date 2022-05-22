434ae48b3b537f128143af4fa2bdce6dce489273
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-05-22 20:47:25 +0530

    [Bhagya]calculate wage daily

--- empwage.sh
+++ empwage.sh
@@ -2,12 +2,15 @@
 
 present=1
 absent=0
+wageperhour=20
+fulldayhour=8
 empstatus=$((RANDOM % 2))
 
 echo "Welcome to Emplopyee wage computation "
 
 if [ $present -eq $empstatus ]
 then
+	wageperday=$(($fulldayhour*$wageperhour))
 	echo "employee is present"
 elif [ $absent -eq $empstatus ]
 then
