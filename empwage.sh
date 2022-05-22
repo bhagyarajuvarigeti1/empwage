535bd1b207a4f1c3a6d7e1b44e10449954d3669b
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-05-22 20:17:06 +0530

    [Bhagya]check employee is present or absent

--- empwage.sh
+++ empwage.sh
@@ -1,3 +1,16 @@
 #! /bin/bash -x
 
-echo "Welcome to Emplopyee"
+present=1
+absent=0
+empstatus=$((RANDOM % 2))
+
+echo "Welcome to Emplopyee wage computation "
+
+if [ $present -eq $empstatus ]
+then
+	echo "employee is present"
+elif [ $absent -eq $empstatus ]
+then
+	echo "employee is absent"
+fi
+
